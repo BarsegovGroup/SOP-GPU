c6c4037b26addee4f822392ccfb5e2efb7aaf9d7
Andrey Alekseenko <al42and@gmail.com>
2015-04-22 15:18:24 +0300

    Using default sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,6 +122,8 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_style = 'custom.css'
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
